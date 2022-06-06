9a3878e70ad37dd138f111e2d9df3d123614fff1
zzmgoing <zzming@zzming.cn>
2022-06-06 22:41:32 +0800

    添加获取版本号方法

--- library/src/main/java/com/zzming/core/common/LibConfig.kt
+++ library/src/main/java/com/zzming/core/common/LibConfig.kt
@@ -1,5 +1,7 @@
 package com.zzming.core.common
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.zzming.core.BuildConfig
 import com.zzming.core.LibCore
 
@@ -26,10 +28,27 @@ object LibConfig {
     lateinit var packageName: String
 
     /**
+     * 应用版本
+     */
+    lateinit var packageVersionName: String
+
+    /**
+     * 应用版本
+     */
+    var packageVersionCode: Long = 0
+
+    /**
      * 初始化配置字段
      */
     fun init() {
         packageName = LibCore.context.packageName
+        val packageInfo = LibCore.context.packageManager.getPackageInfo(packageName, 0)
+        packageVersionName = packageInfo.versionName
+        packageVersionCode = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            packageInfo.longVersionCode
+        } else {
+            packageInfo.versionCode.toLong()
+        }
     }
 
 
